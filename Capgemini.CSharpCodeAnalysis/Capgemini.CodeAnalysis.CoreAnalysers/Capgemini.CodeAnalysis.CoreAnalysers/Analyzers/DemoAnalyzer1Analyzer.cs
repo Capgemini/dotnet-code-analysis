ffc3f7d30606a8d3a91987f024eee89e46dbbac8
jay.barden <jay.barden@capgemini.com>
2018-07-18 09:15:14 +0100

    Not our base-class either...

--- Capgemini.CSharpCodeAnalysis/Capgemini.CodeAnalysis.CoreAnalysers/Capgemini.CodeAnalysis.CoreAnalysers/Analyzers/DemoAnalyzer1Analyzer.cs
+++ Capgemini.CSharpCodeAnalysis/Capgemini.CodeAnalysis.CoreAnalysers/Capgemini.CodeAnalysis.CoreAnalysers/Analyzers/DemoAnalyzer1Analyzer.cs
@@ -13,14 +13,13 @@ using Microsoft.CodeAnalysis.Diagnostics;
 namespace Capgemini.CodeAnalysis.CoreAnalysers.Analyzers
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class DemoAnalyzer1Analyzer : DiagnosticAnalyzer
+    public class DemoAnalyzer1Analyzer : AnalyzerBase
     {
         // public const string DiagnosticId = "DemoAnalyzer1_JJB";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Naming";
 
